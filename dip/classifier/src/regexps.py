38c2302bbf81ceb7a880a4bb213a769706bc7a5b
Tom Marek <angelo.sharpe@gmail.com>
2012-11-23 23:45:58 +0100

    Fix URL and smiles regexps
    Fix URL regexp to ignore trailing dots or brackets.
    Add some new smiles
    
    Signed-off-by: Tom Marek <angelo.sharpe@gmail.com>

--- dip/classifier/src/regexps.py
+++ dip/classifier/src/regexps.py
@@ -2,13 +2,13 @@ import re
 
 ### EMOTICONS ###
 # facee moticon definitions
-anger = r'>?'
-eyes = r'[:=8X;]'
+top = r'[<>O}\]3P]?'
+eyes = r'[:=Xx;]'
 tear = r'\'?'
 nose = r'[oO-]?'
-# d, D, ), ], /, S, (. [, \, |, p, P, o, O
-mouths = r'[dD\)\]/S\(\[\\\|pPoO]'
-faces = ''.join([anger, eyes, tear, nose, mouths])
+# d, D, ), ], /, S, (. [, \, |, p, P, o, O, c, C, @, 3, &
+mouths = r'[dD\)\]/S\(\[\\\|pPoOcC@{}3&]'
+faces = ''.join([top, eyes, tear, nose, mouths])
 # other emoticons definitions
 other_emoticons = [
     r'\^_+\^', r'o\.O', r'@_+@', r'-_+-',
@@ -21,20 +21,19 @@ emoticons_re = re.compile(
 
 ### URLs ###
 urls_re = re.compile(
-    r'\s(https?|s?ftp)(://)(www\.)?([\w\.-]+\.[a-zA-Z]{2,4})(:\d*)?' +
-    r'(/[-_~$.+!*\'()\[\],;:@&=\?/~#%\w#]*)' +
+    r'(https?|s?ftp)(://)(www\.)?([\w\.-]+\.[a-zA-Z]{2,4})(:\d*)?' +
+    r'(/[-_~$.+!*\'()\[\],;:@&=\?/~#%\w#]*)[^\.\,\)\(\s]' +
     r'|' +
     r'(www\.)([\w\d\.-]+\.[a-zA-Z]{2,4})(:\d*)?' +
-    r'(/[-_~$.+!*\'()\[\],;:@&=\?/~#%\w#]*)'
+    r'(/[-_~$.+!*\'()\[\],;:@&=\?/~#%\w#]*)[^\.\,\)\(\s]'
 )
 
-
 ### emails ###
-emails_re = re.compile(r'\s([\w_\.-]+)(@)([\w\.-]+)(\.)([a-zA-Z]{2,6})\s')
+emails_re = re.compile(r'\s([\w_\.-]+)(@)([\w\.-]+)(\.)([a-zA-Z]{2,6})')
 
 
 ### tweet tags ###
-tags_re = re.compile(r'\s(#\w+)\s')
+tags_re = re.compile(r'(#\w+)')
 
 
 ### Timelike ###
